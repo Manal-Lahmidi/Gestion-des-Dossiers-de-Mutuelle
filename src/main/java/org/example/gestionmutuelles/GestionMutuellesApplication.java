dda6af41a0a68458c1ca410ab29246a87422c65e
Manal-Lahmidi <manallahmidi40@gmail.com>
2024-11-21 21:22:45 +0100

    test

---
+++ src/main/java/org/example/gestionmutuelles/GestionMutuellesApplication.java
@@ -0,0 +1,13 @@
+package org.example.gestionmutuelles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionMutuellesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionMutuellesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/gestionmutuelles/GestionMutuellesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gestionmutuelles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionMutuellesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
